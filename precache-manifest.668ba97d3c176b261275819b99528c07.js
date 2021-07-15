12498f5aeec966ca2858a983504b567ee31bfae8
Ben Knize <bknize@gmail.com>
2021-07-14 20:50:08 -0500

    Updates

---
+++ precache-manifest.668ba97d3c176b261275819b99528c07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa0bf16e0ebe63a33ed1cd9bbf684ab",
+    "url": "/bknize.github.io/index.html"
+  },
+  {
+    "revision": "51d31fcff9cfa4689e47",
+    "url": "/bknize.github.io/static/css/main.80ba7e9a.chunk.css"
+  },
+  {
+    "revision": "459fe2b797be91d4b134",
+    "url": "/bknize.github.io/static/js/2.9c54a296.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bknize.github.io/static/js/2.9c54a296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d31fcff9cfa4689e47",
+    "url": "/bknize.github.io/static/js/main.d967014b.chunk.js"
+  },
+  {
+    "revision": "4d10c5a55beddae6775d",
+    "url": "/bknize.github.io/static/js/runtime-main.1333fc52.js"
+  }
+]);
